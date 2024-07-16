2c9fbfb52cbaf08ce0175825ed12a254c1906165
M Aslam <aslam4webz@gmail.com>
2024-07-16 20:02:17 +0100

    Added Resume, Services, Footer, and Contact components to the Dashboard page with styling updates.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,10 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import Resume from './Components/Resume.vue';
+import Services from './Components/Services.vue';
+import Footer from './Components/Footer.vue';
+import Contact from './Components/Contact.vue'
 </script>
 
 <template>
@@ -8,17 +12,23 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <div class="px-20 py-6">
+        <div class="py-6 context">
+
+
             <!-- hero section -->
-            <div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
-                <div class="text-6xl font-semibold text-gray-900 leading-none">Bring all your work
-                    together</div>
-                <div class="mt-6 text-xl font-light text-true-gray-500 antialiased">A better experience
-                    for yout attendees and less stress yout team.</div>
-                <button
+            <div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-30 text-left">
+                <div class="text-2xl font-semibold text-[#728afb] leading-none">Greetings! 👋 I'm</div>
+                <div class="text-6xl font-semibold text-gray-900 leading-none">Mohamed Aslam</div>
+                <div class="text-3xl font-semibold text-gray-900 leading-none">Full Stack developer</div>
+                <div class="mt-6 text-xl font-light text-[#9ea2b6] antialiased">Driven by a passion for innovation
+                    and backed by years of experience, I specialize in the intricate world of back-end development. From
+                    crafting efficient databases to optimizing server-side processes, I'm committed to building the
+                    foundation that powers exceptional digital solutions. Let's collaborate and turn your vision into
+                    reality!</div>
+                <a href="#contact"
                     class="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-gradient-to-b from-blue-600 to-blue-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
-                    Download for Free
-                </button>
+                    Contact Me
+                </a>
             </div>
             <div class="mt-12 lg:mt-32 lg:ml-20 text-left">
                 <bottom type="button"
@@ -31,8 +41,166 @@ import { Head } from '@inertiajs/vue3';
                 </bottom>
             </div>
 
-            <!-- /hero section -->
+            <!-- Services -->
+            <Services></Services>
 
+            <!-- Resume Experience Section -->
+            <Resume></Resume>
+
+            <!-- Resume Experience Section -->
+            <Contact></Contact>
+            <!-- Footer -->
+            <Footer></Footer>
         </div>
+        <div class="area" >
+            <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div >
     </AuthenticatedLayout>
 </template>
+
+<style scoped>
+.context {
+    width: 100%;
+    position: absolute;
+    top:50vh;
+}
+
+.context h1{
+    text-align: center;
+    color: #fff;
+    font-size: 50px;
+}
+
+.area{
+    background: #4e54c8;
+    background: -webkit-linear-gradient(to left, #8f94fb, #4e54c8);
+    width: 100%;
+    height:100vh;
+}
+
+.circles{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+
+.circles li{
+    position: absolute;
+    display: block;
+    list-style: none;
+    width: 20px;
+    height: 20px;
+    background: rgba(255, 255, 255, 0.2);
+    animation: animate 25s linear infinite;
+    bottom: -150px;
+
+}
+
+.circles li:nth-child(1){
+    left: 25%;
+    width: 80px;
+    height: 80px;
+    animation-delay: 0s;
+}
+
+
+.circles li:nth-child(2){
+    left: 10%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 2s;
+    animation-duration: 12s;
+}
+
+.circles li:nth-child(3){
+    left: 70%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 4s;
+}
+
+.circles li:nth-child(4){
+    left: 40%;
+    width: 60px;
+    height: 60px;
+    animation-delay: 0s;
+    animation-duration: 18s;
+}
+
+.circles li:nth-child(5){
+    left: 65%;
+    width: 20px;
+    height: 20px;
+    animation-delay: 0s;
+}
+
+.circles li:nth-child(6){
+    left: 75%;
+    width: 110px;
+    height: 110px;
+    animation-delay: 3s;
+}
+
+.circles li:nth-child(7){
+    left: 35%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 7s;
+}
+
+.circles li:nth-child(8){
+    left: 50%;
+    width: 25px;
+    height: 25px;
+    animation-delay: 15s;
+    animation-duration: 45s;
+}
+
+.circles li:nth-child(9){
+    left: 20%;
+    width: 15px;
+    height: 15px;
+    animation-delay: 2s;
+    animation-duration: 35s;
+}
+
+.circles li:nth-child(10){
+    left: 85%;
+    width: 150px;
+    height: 150px;
+    animation-delay: 0s;
+    animation-duration: 11s;
+}
+
+
+
+@keyframes animate {
+
+    0%{
+        transform: translateY(0) rotate(0deg);
+        opacity: 1;
+        border-radius: 0;
+    }
+
+    100%{
+        transform: translateY(-1000px) rotate(720deg);
+        opacity: 0;
+        border-radius: 50%;
+    }
+
+}
+</style>
