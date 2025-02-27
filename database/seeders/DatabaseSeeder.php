3e091171cca619d880152272a164e43e6c580139
M Aslam <aslam4webz@gmail.com>
2025-02-27 23:41:35 +0000

    fixed spelling issues

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             HMCategorySeeder::class,
-            restaurantSeeder::class,
+            RestaurantSeeder::class,
             ProductSeeder::class,
         ]);
     }
